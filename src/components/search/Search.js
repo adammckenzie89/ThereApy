ae77c54a0559970f05e2a58625463b754f4ec016
adam mckenzie <adammckenzie@adams-MacBook-Pro-2.local>
2019-05-14 23:10:11 -0500

    night before

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -5,6 +5,14 @@ import axios from "axios";
 import Header from "../header/Header";
 import styles from "./search.module.scss";
 import Spinner from "react-spinkit";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+toast.configure();
+
+toast.configure({
+  autoClose: 8000,
+  draggable: false
+});
 
 const { REACT_APP_KEY } = process.env;
 
@@ -52,6 +60,7 @@ class Search extends Component {
         );
       });
   };
+  notify = () => toast("Added to favorites", { containerID: "B" });
 
   render() {
     let displayData = this.state.data.map((val, index) => {
@@ -73,19 +82,19 @@ class Search extends Component {
                 <h3> {val.result.result.rating} </h3>
               </div>
               <section>
-                {/* className={styles.heart} */}
                 <svg
                   className={styles.heart}
                   width="24"
                   height="24"
                   xmlns="http://www.w3.org/2000/svg"
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
                 >
                   <path
                     fill="red"
                     d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"
                     onClick={() => {
+                      {
+                        this.notify();
+                      }
                       axios
                         .post("/api/addFavorite", {
                           name: val.result.result.name,
@@ -114,8 +123,12 @@ class Search extends Component {
     });
     return (
       <div>
+        <ToastContainer
+          enableMultiContainer
+          containerId={"B"}
+          position={toast.POSITION.TOP_RIGHT}
+        />
         <Header />
-
         <div className={styles.bigContainer}>
           <main className={styles.main}>
             <section className={styles.welcomeUser}>
